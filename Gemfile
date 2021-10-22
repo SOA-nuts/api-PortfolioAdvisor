0a7559fad193b6fffc005dad22fe6b44d4ac7c27
ChungYiTsai <jid050887@gmail.com>
2021-10-23 01:05:32 +0800

    finish part 3 and 4

--- Gemfile
+++ Gemfile
@@ -4,11 +4,20 @@ source 'https://rubygems.org'
 # Networking
 gem 'http', '~> 5.0'
 
+# main api
 gem 'news-api'
-gem 'reek'
 
 # Testing
 gem 'minitest', '~> 5.0'
 gem 'minitest-rg', '~> 5.0'
+gem 'simplecov', '~> 0'
 gem 'vcr', '~> 6.0'
 gem 'webmock', '~> 3.0'
+
+# Utility Tools
+gem 'rake'
+
+# Code Quality
+gem 'flog'
+gem 'reek'
+gem 'rubocop'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start
+
 require 'minitest/autorun'
 require 'minitest/rg'
 require 'yaml'
@@ -10,9 +13,9 @@ require_relative '../lib/google_news_api'
 
 TOPIC = 'business'
 RESULT_NUM = 15
-CONFIG = YAML.safe_load(File.read('../config/secrets.yml'))
+CONFIG = YAML.safe_load(File.read('config/secrets.yml'))
 GOOGLENEWS_TOKEN = CONFIG['GOOGLENEWS_TOKEN']
-CORRECT = YAML.safe_load(File.read('../spec/fixtures/business_results.yml'))
+CORRECT = YAML.safe_load(File.read('spec/fixtures/business_results.yml'))
 
-CASSETTES_FOLDER = 'fixtures/cassettes'
+CASSETTES_FOLDER = 'spec/fixtures/cassettes'
 CASSETTE_FILE = 'google_news_api'
