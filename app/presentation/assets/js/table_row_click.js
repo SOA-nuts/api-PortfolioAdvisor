87c8f0019c8def936b0a3ac46e7605d66b9811a1
ShinRuHung <cin810619121dy@gmail.com>
2021-11-26 22:04:55 +0800

    add line chart on history page

--- app/presentation/assets/js/table_row_click.js
+++ app/presentation/assets/js/table_row_click.js
@@ -7,3 +7,42 @@ $(document).ready(function() {
       window.open($(this).data("href"));
     });
   });
+
+  window.onload = function(){
+    var update_time =[]
+    var score = []
+    var index = 0;
+    $("table#history_table tr").each(function() {
+        var arrayOfThisRow = [];
+        var tableData = $(this).find('td');
+        
+        if (tableData.length > 0) {
+            tableData.each(function() { arrayOfThisRow.push($(this).text()); });
+            update_time[index] = arrayOfThisRow[0];
+            score[index] = arrayOfThisRow[1];
+            index++;
+        }
+     });
+
+     var ctxL = $(".LineChart")
+     var myLineChart = new Chart(ctxL, {
+       type: 'line',      
+       data: {
+         labels: update_time,
+         datasets: [{
+           backgroundColor: "rgba(0,0,0,1.0)",
+           borderColor: "rgba(0,0,0,0.1)",
+           data: score
+         }]
+       },
+       options: {
+         responsive: true
+       }
+     });
+     
+    }
+    // var chart = BuildChart(labels, myTableArray, "Items Sold Over Time");
+
+    
+
+
