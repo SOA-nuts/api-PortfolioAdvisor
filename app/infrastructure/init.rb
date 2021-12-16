77311b298f73b105afaa32fd6d1c4e38a04ee879
ChungYiTsai <jid050887@gmail.com>
2021-12-16 18:39:49 +0800

    add concurrency

--- app/infrastructure/init.rb
+++ app/infrastructure/init.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-folders = %w[google_news crawler database cache]
+folders = %w[google_news crawler database cache messaging]
 folders.each do |folder|
   require_relative "#{folder}/init.rb"
 end
