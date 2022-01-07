9993660b6d77377352fddaeef6cc1e56b2150fee
ChungYiTsai <jid050887@gmail.com>
2022-01-07 18:38:10 +0800

    app.config

--- app/infrastructure/messaging/queue.rb
+++ app/infrastructure/messaging/queue.rb
@@ -12,9 +12,9 @@ module PortfolioAdvisor
       def initialize(queue_url, config)
         @queue_url = queue_url
         sqs = Aws::SQS::Client.new(
-          access_key_id: config.AWS_ACCESS_KEY_ID,
-          secret_access_key: config.AWS_SECRET_ACCESS_KEY,
-          region: config.AWS_REGION
+          access_key_id: App.config.AWS_ACCESS_KEY_ID,
+          secret_access_key: App.config.AWS_SECRET_ACCESS_KEY,
+          region: App.config.AWS_REGION
         )
         @queue = Aws::SQS::Queue.new(url: queue_url, client: sqs)
       end
